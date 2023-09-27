aa2aa845d26d9e1f024386d6d4982c9cff7f1cae
sam-mcginn <samantha_mcginn@student.uml.edu>
2023-09-27 17:08:07 -0400

    py edit

--- packages/homework_two/src/hw_two_pub.py
+++ packages/homework_two/src/hw_two_pub.py
@@ -10,15 +10,42 @@ import time
 class Talker:
     def __init__(self):
         self.pub=rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=10)
+        i=1
 
     def talk(self):
         move_msg=Twist()
-        move_msg.linear.x=1.0
-        move_msg.linear.y=1.0
-        move_msg.linear.z=0.0
-        move_msg.angular.x=0.0
-        move_msg.angular.y=0.0
-        move_msg.angular.z=0.0
+        if i==1
+            move_msg.linear.x=1.0
+            move_msg.linear.y=0.0
+            move_msg.linear.z=0.0
+            move_msg.angular.x=0.0
+            move_msg.angular.y=0.0
+            move_msg.angular.z=0.0
+            i=2
+        elif i==2
+            move_msg.linear.x=0.0
+            move_msg.linear.y= -1.0
+            move_msg.linear.z=0.0
+            move_msg.angular.x=0.0
+            move_msg.angular.y=0.0
+            move_msg.angular.z=0.0
+            i=3
+        elif i==3
+            move_msg.linear.x= -1.0
+            move_msg.linear.y=0.0
+            move_msg.linear.z=0.0
+            move_msg.angular.x=0.0
+            move_msg.angular.y=0.0
+            move_msg.angular.z=0.0
+            i=4
+        else
+            move_msg.linear.x=0.0
+            move_msg.linear.y=1.0
+            move_msg.linear.z=0.0
+            move_msg.angular.x=0.0
+            move_msg.angular.y=0.0
+            move_msg.angular.z=0.0
+            i=1
         self.pub.publish(move_msg)
         
 if __name__ == "__main__":
