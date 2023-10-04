a73ee1864fa6b0bb161bacdb27c78bd1a5ec8264
sam-mcginn <samantha_mcginn@student.uml.edu>
2023-10-04 17:20:55 -0400

    debug

--- packages/homework_two/src/hw3_node.py
+++ packages/homework_two/src/hw3_node.py
@@ -10,32 +10,32 @@ from turtlesim.msg import Pose
 class MeasureDist:
     def __init__(self):
         rospy.Subscriber('turtle1/pose', Pose, self.callback)
-        self.pub = rospy.Publisher('turtle1/dist_traveled', UnitsLabelled, queue_size=10)
-        self.curr_x = 0.0
-        self.curr_y = 0.0
-        self.prev_x = 0.0
-        self.prev_y = 0.0
-        self.start_flag = 0
-        self.dist_total = 0.0
-        self.dist_msg = UnitsLabelled()
-        self.dist_msg.units = "meters"
+        #self.pub = rospy.Publisher('turtle1/dist_traveled', UnitsLabelled, queue_size=10)
+        #self.curr_x = 0.0
+        #self.curr_y = 0.0
+        #self.prev_x = 0.0
+        #self.prev_y = 0.0
+        #self.start_flag = 0
+        #self.dist_total = 0.0
+        #self.dist_msg = UnitsLabelled()
+        #self.dist_msg.units = "meters"
     
     def callback(self, msg):
         # update current and previous position
-        if self.start_flag == 0:
-            self.curr_x = msg.x
-            self.curr_y = msg.y
-            self.start_flag = 1
-        self.prev_x = self.curr_x
-        self.prev_y = self.curr_y
-        self.curr_x = msg.x
-        self.curr_y = msg.y
+        #if self.start_flag == 0:
+            #self.curr_x = msg.x
+            #self.curr_y = msg.y
+            #self.start_flag = 1
+        #self.prev_x = self.curr_x
+        #self.prev_y = self.curr_y
+        #self.curr_x = msg.x
+        #self.curr_y = msg.y
         # calculate and publish distance traveled
-        self.dist_total += math.sqrt( (self.curr_x - self.prev_x)**2 + \
-        (self.curr_y - self.prev_y)**2 )
+        #self.dist_total += math.sqrt( (self.curr_x - self.prev_x)**2 + \
+        #(self.curr_y - self.prev_y)**2 )
         #print(dist_total)
-        self.dist_msg.value = self.dist_total
-        self.pub.publish(dist_msg)
+        #self.dist_msg.value = self.dist_total
+        #self.pub.publish(dist_msg)
         
         
     
