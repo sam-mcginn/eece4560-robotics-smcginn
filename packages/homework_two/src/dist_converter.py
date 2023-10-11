f85e2b7434f616d7d57b4f0162dadd27ce72f9d3
sam-mcginn <samantha_mcginn@student.uml.edu>
2023-10-10 20:13:55 -0400

    fixed topic name

--- packages/homework_two/src/dist_converter.py
+++ packages/homework_two/src/dist_converter.py
@@ -9,7 +9,7 @@ from std_msgs.msg import String
 class DistanceConverter:
     def __init__(self):
         self.curr_unit = "smoots"
-        rospy.Subscriber('turtle1/dist_measured', UnitsLabelled, self.convert)
+        rospy.Subscriber('turtle1/dist_traveled', UnitsLabelled, self.convert)
         self.pub = rospy.Publisher('turtle1/dist_converted', UnitsLabelled, queue_size=10)
         rospy.Subscriber('turtle1/conversion_unit', String, self.change_unit)
     
