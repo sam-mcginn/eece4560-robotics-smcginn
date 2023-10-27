0be435b7fa65cf82d9efb53498369b20ca8ce650
sam-mcginn <samantha_mcginn@student.uml.edu>
2023-10-26 21:28:01 -0400

    removed pub from launch, added transform calcs

--- packages/coord_transform/src/sensor_coord_transform.py
+++ packages/coord_transform/src/sensor_coord_transform.py
@@ -5,16 +5,24 @@ import math
 from duckietown_msgs.msg import Vector2D
 # Vector2D: float32 x,y
 
+angle_wtr = (5.0*math.pi)/6.0
+angle_rts = math.pi
+
 class SensorTransform:
-    def __init__(self):
+    def __init__(self):s
         rospy.Subscriber('/sensor_coord', Vector2D, self.transform)
         self.pub_rc = rospy.Publisher('/robot_coord', Vector2D, queue_size=10)
         self.pub_wc = rospy.Publisher('/world_coord', Vector2D, queue_size=10)
         self.curr_scoord = numpy.matrix([[0],[0],[1]])
         self.curr_rcoord = numpy.matrix([[0],[0],[1]])
         self.curr_wcoord = numpy.matrix([[0],[0],[1]])
-        self.wts_xform = numpy.matrix([[0.866, 0.5, 6.866], [-0.5, 0.866, 1.5], [0.0,0.0,1.0]])
-        self.rts_xform = numpy.matrix([[-1.0,0.0,-1.0], [0.0,-1.0,0.0], [0,0,1]])
+        self.init_transforms()
+        
+    def init_transforms(self):
+        self.wtr_xform = numpy.matrix([[math.cos(angle_wtr), -1.0*math.sin(angle_wtr), 6.0], [math.sin(angle(wtr), math.cos(angle_wtr), 2.0], [0.0, 0.0, 1.0]])
+        self.rts_xform = numpy.matrix([[math.cos(angle_rts), -1.0*math.sin(angle_rts), -1.0], [math.sin(angle(rts), math.cos(angle_rts), 0.0], [0.0, 0.0, 1.0]])
+        self.wts_xform = wtr_xform*rts_xform
+    
         
     def transform(self, sensor_vector):
         # initialize msgs
