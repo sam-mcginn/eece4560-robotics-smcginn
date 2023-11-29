d1b81cd04187a6086e951815d5b9643fbee50322
sam-mcginn <samantha_mcginn@student.uml.edu>
2023-11-29 14:27:13 -0500

    adjusted params

--- packages/edge_detection/src/edge_detect.py
+++ packages/edge_detection/src/edge_detect.py
@@ -47,8 +47,8 @@ class Edge_Detect:
          
         # Do Hough transform on both ANDed images
         # cv2.HoughLinesP( img, rho, theta, threshold, minLineLength, maxLineGap)
-        self.yt_hough = cv2.HoughLinesP(self.yt_edges, 1, numpy.pi/180, 25, 50, 25)
-        self.ylw_hough = cv2.HoughLinesP(self.ylw_edges, 1, numpy.pi/180, 25, 50, 25)
+        self.yt_hough = cv2.HoughLinesP(self.yt_edges, 1, numpy.pi/180, 25, 25, 25)
+        self.ylw_hough = cv2.HoughLinesP(self.ylw_edges, 1, numpy.pi/180, 25, 25, 25)
          
         # Add lines from Hough transform to original cropped image
         # cv2.line(img, start_pt, end_pt, BGR_color, line_thicknes)
