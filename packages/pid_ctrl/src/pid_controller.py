d931830acb2ba4d2ba85fd7f2851d485815f4c95
sam-mcginn <samantha_mcginn@student.uml.edu>
2023-12-06 12:12:49 -0500

    outlined hw9 pid ctrl

--- packages/pid_ctrl/src/pid_controller.py
+++ packages/pid_ctrl/src/pid_controller.py
@@ -2,3 +2,45 @@
 import rospy
 import math
 from std_msgs import Float32
+
+class PID_Ctrl:
+    def __init__(self):
+        # Gain parameters
+        self.kp = 0;
+        self.ki = 0;
+        self.kd = 0;
+        
+        self.prev_error;
+        self.error_total;
+        
+        # Publish to controller, subscribe to error
+        self.pub = rospy.Publisher('control_input', Float32, queue_size=10)
+        rospy.Subscriber('error', Float32, self.callback_pid)
+        
+    def callback_pid(self, error):
+        dt = 1
+        
+        # Calculate proportional
+        proportional = self.kp*error
+        
+        # Calculate integral
+        # FIX
+        self.error_total = self.error_total + error
+        integral = (self.ki)*(self.error_total)*dt
+        
+        
+        # Calculate derivative
+        # FIX
+        derivative = (self.kd)*(error - self.prev_error)/(dt)
+        
+        gain = Float32()
+        gain = proportional + integral + derivative
+        self.pub.publish(gain)
+        
+if __name__ == '__main__':
+    rospy.init_node('sensor_transform', anonymous=True)
+    PID_Ctrl()
+    rospy.spin()
+
+
+
